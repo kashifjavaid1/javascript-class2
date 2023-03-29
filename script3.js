6348377cbbcc89cad851df0cf3e2c92220b81870
kashifjavaid1 <gmdcf797@gmail.com>
2023-03-29 14:04:26 +0500

    2 varaible create javascript
    varaibel create name1 and num2 check data type and sum off 2 numbers

---
+++ script3.js
@@ -0,0 +1,12 @@
+// 2 varaibel create
+var name1="kashif";
+var num2="2.452";
+console.log("Result show =",name1 ,typeof(name1));
+num2=Number(num2);
+
+console.log("result",num2, typeof(num2));
+
+console.log("Result show =",nam,typeof(nam));
+console.log("Result show =",j,typeof(j));
+
+
